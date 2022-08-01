220ee5770c7724ba006adaf2e652a3d9bf3e49d6
Didier Gourillon <dgourillon@google.com>
2022-08-01 18:20:32 +0200

    comment out - all groups but org admin

--- fast-fabric-groups.tf
+++ fast-fabric-groups.tf
@@ -11,46 +11,46 @@ module "group_organization-admins" {
   members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
 }
 
-module "group_gcp-devops" {
-  source  = "terraform-google-modules/group/google"
-  version = "~> 0.4"
+# module "group_gcp-devops" {
+#   source  = "terraform-google-modules/group/google"
+#   version = "~> 0.4"
 
-  id           = "gcp-gcp-devops@dgourillon1.joonix.net"
-  display_name = "gcp-gcp-devops"
-  description  = "gcp-gcp-devops"
-  domain       = "dgourillon1.joonix.net"
-  members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
-}
+#   id           = "gcp-gcp-devops@dgourillon1.joonix.net"
+#   display_name = "gcp-gcp-devops"
+#   description  = "gcp-gcp-devops"
+#   domain       = "dgourillon1.joonix.net"
+#   members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
+# }
 
-module "group_network-admins" {
-  source  = "terraform-google-modules/group/google"
-  version = "~> 0.4"
+# module "group_network-admins" {
+#   source  = "terraform-google-modules/group/google"
+#   version = "~> 0.4"
 
-  id           = "gcp-network-admins@dgourillon1.joonix.net"
-  display_name = "gcp-network-admins"
-  description  = "gcp-network-admins"
-  domain       = "dgourillon1.joonix.net"
-  members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
-}
+#   id           = "gcp-network-admins@dgourillon1.joonix.net"
+#   display_name = "gcp-network-admins"
+#   description  = "gcp-network-admins"
+#   domain       = "dgourillon1.joonix.net"
+#   members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
+# }
 
-module "group_security-admins" {
-  source  = "terraform-google-modules/group/google"
-  version = "~> 0.4"
+# module "group_security-admins" {
+#   source  = "terraform-google-modules/group/google"
+#   version = "~> 0.4"
 
-  id           = "gcp-security-admins@dgourillon1.joonix.net"
-  display_name = "gcp-security-admins"
-  description  = "gcp-security-admins"
-  domain       = "dgourillon1.joonix.net"
-  members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
-}
+#   id           = "gcp-security-admins@dgourillon1.joonix.net"
+#   display_name = "gcp-security-admins"
+#   description  = "gcp-security-admins"
+#   domain       = "dgourillon1.joonix.net"
+#   members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
+# }
 
-module "group_support" {
-  source  = "terraform-google-modules/group/google"
-  version = "~> 0.4"
+# module "group_support" {
+#   source  = "terraform-google-modules/group/google"
+#   version = "~> 0.4"
 
-  id           = "gcp-support@dgourillon1.joonix.net"
-  display_name = "gcp-support"
-  description  = "gcp-support"
-  domain       = "dgourillon1.joonix.net"
-  members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
-}
+#   id           = "gcp-support@dgourillon1.joonix.net"
+#   display_name = "gcp-support"
+#   description  = "gcp-support"
+#   domain       = "dgourillon1.joonix.net"
+#   members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
+# }
