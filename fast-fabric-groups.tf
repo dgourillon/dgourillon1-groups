ab8ccdb21ca7a1154da2a33697c6765854bf635f
Didier Gourillon <dgourillon@google.com>
2022-08-01 18:32:02 +0200

    uncomment all groups

--- fast-fabric-groups.tf
+++ fast-fabric-groups.tf
@@ -6,51 +6,55 @@ module "group_organization-admins" {
   id           = "gcp-organization-admins@dgourillon1.joonix.net"
   display_name = "gcp-organization-admins"
   description  = "gcp-organization-admins"
-  owners       = ["tf-cloud-sa@ci-groups-dgo1.iam.gserviceaccount.com"]
+  owners       = ["tf-cloud-sa@ci-groups-dgo1.iam.gserviceaccount.com","dgourillon@dgourillon1.joonix.net"]
   domain       = "dgourillon1.joonix.net"
   members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
 }
 
-# module "group_gcp-devops" {
-#   source  = "terraform-google-modules/group/google"
-#   version = "~> 0.4"
+module "group_gcp-devops" {
+  source  = "terraform-google-modules/group/google"
+  version = "~> 0.4"
 
-#   id           = "gcp-gcp-devops@dgourillon1.joonix.net"
-#   display_name = "gcp-gcp-devops"
-#   description  = "gcp-gcp-devops"
-#   domain       = "dgourillon1.joonix.net"
-#   members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
-# }
+  id           = "gcp-gcp-devops@dgourillon1.joonix.net"
+  display_name = "gcp-gcp-devops"
+  description  = "gcp-gcp-devops"
+  owners       = ["tf-cloud-sa@ci-groups-dgo1.iam.gserviceaccount.com","dgourillon@dgourillon1.joonix.net"]
+  domain       = "dgourillon1.joonix.net"
+  members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
+}
 
-# module "group_network-admins" {
-#   source  = "terraform-google-modules/group/google"
-#   version = "~> 0.4"
+ module "group_network-admins" {
+  source  = "terraform-google-modules/group/google"
+  version = "~> 0.4"
 
-#   id           = "gcp-network-admins@dgourillon1.joonix.net"
-#   display_name = "gcp-network-admins"
-#   description  = "gcp-network-admins"
-#   domain       = "dgourillon1.joonix.net"
-#   members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
-# }
+  id           = "gcp-network-admins@dgourillon1.joonix.net"
+  display_name = "gcp-network-admins"
+  description  = "gcp-network-admins"
+  owners       = ["tf-cloud-sa@ci-groups-dgo1.iam.gserviceaccount.com","dgourillon@dgourillon1.joonix.net"]
+  domain       = "dgourillon1.joonix.net"
+  members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
+}
 
-# module "group_security-admins" {
-#   source  = "terraform-google-modules/group/google"
-#   version = "~> 0.4"
+module "group_security-admins" {
+  source  = "terraform-google-modules/group/google"
+  version = "~> 0.4"
 
-#   id           = "gcp-security-admins@dgourillon1.joonix.net"
-#   display_name = "gcp-security-admins"
-#   description  = "gcp-security-admins"
-#   domain       = "dgourillon1.joonix.net"
-#   members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
-# }
+  id           = "gcp-security-admins@dgourillon1.joonix.net"
+  display_name = "gcp-security-admins"
+  description  = "gcp-security-admins"
+  owners       = ["tf-cloud-sa@ci-groups-dgo1.iam.gserviceaccount.com","dgourillon@dgourillon1.joonix.net"]
+  domain       = "dgourillon1.joonix.net"
+  members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
+}
 
-# module "group_support" {
-#   source  = "terraform-google-modules/group/google"
-#   version = "~> 0.4"
+module "group_support" {
+  source  = "terraform-google-modules/group/google"
+  version = "~> 0.4"
 
-#   id           = "gcp-support@dgourillon1.joonix.net"
-#   display_name = "gcp-support"
-#   description  = "gcp-support"
-#   domain       = "dgourillon1.joonix.net"
-#   members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
-# }
+  id           = "gcp-support@dgourillon1.joonix.net"
+  display_name = "gcp-support"
+  description  = "gcp-support"
+  owners       = ["tf-cloud-sa@ci-groups-dgo1.iam.gserviceaccount.com","dgourillon@dgourillon1.joonix.net"]
+  domain       = "dgourillon1.joonix.net"
+  members      = ["dgourillon@dgourillon1.joonix.net","dgourillon-aliases@dgourillon1.joonix.net"]
+}
