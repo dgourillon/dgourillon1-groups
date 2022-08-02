89f307ee4f2ac700c249a5d4d5fa5fe154a46f30
Didier Gourillon <dgourillon@google.com>
2022-08-02 14:29:10 +0200

    fix duplicate values

--- team-a-groups.tf
+++ team-a-groups.tf
@@ -4,7 +4,7 @@ locals {
 
 }
 
-module "fast_fabric_groups" {
+module "team_a_groups" {
   source  = "terraform-google-modules/group/google"
   version = "~> 0.4"
   for_each = toset(local.group_list)
@@ -20,7 +20,7 @@ module "fast_fabric_groups" {
   ]
 }
 
-resource "googleworkspace_user" "fast_fabric_users" {
+resource "googleworkspace_user" "team_a_users" {
   for_each = toset(local.group_list)
   primary_email = "${each.key}-user@dgourillon1.joonix.net"
   password      = var.default_password
