b248e2f416baa89faca3495ff1dbaaf122ba8819
Didier Gourillon <dgourillon@google.com>
2022-08-02 14:28:17 +0200

    add  team A

--- team-a-groups.tf
+++ team-a-groups.tf
@@ -1,48 +1,33 @@
+locals {
 
-module "group_team-a" {
+  group_list = ["gcp-team-a","gcp-team-a-admin"]
+
+}
+
+module "fast_fabric_groups" {
   source  = "terraform-google-modules/group/google"
   version = "~> 0.4"
+  for_each = toset(local.group_list)
 
-  id           = "team-a@dgourillon1.joonix.net"
-  display_name = "team-a"
-  description  = "team-a"
+  id           = "${each.key}@dgourillon1.joonix.net"
+  display_name = "${each.key}"
+  description  = "${each.key}"
   owners       = ["tf-cloud-sa@ci-groups-dgo1.iam.gserviceaccount.com"]
   domain       = "dgourillon1.joonix.net"
   members      = ["dgourillon-aliases@dgourillon1.joonix.net"]
+  depends_on = [
+    googleworkspace_user.fast_fabric_users
+  ]
 }
 
-resource "googleworkspace_user" "team-a-user" {
-  primary_email = "fast-org-admin@dgourillon1.joonix.net"
+resource "googleworkspace_user" "fast_fabric_users" {
+  for_each = toset(local.group_list)
+  primary_email = "${each.key}-user@dgourillon1.joonix.net"
   password      = var.default_password
 
   name {
-    family_name = "team-a"
-    given_name  = "team-a"
-  }
-
-  recovery_email = "dgourillon@google.com"
-}
-
-
-module "group-team-a-admin" {
-  source  = "terraform-google-modules/group/google"
-  version = "~> 0.4"
-
-  id           = "team-a-admin@dgourillon1.joonix.net"
-  display_name = "team-a-admin"
-  description  = "team-a-admin"
-  owners       = ["tf-cloud-sa@ci-groups-dgo1.iam.gserviceaccount.com"]
-  domain       = "dgourillon1.joonix.net"
-  members      = ["dgourillon-aliases@dgourillon1.joonix.net"]
-}
-
-resource "googleworkspace_user" "team-a-admin-user" {
-  primary_email = "team-a-admin-user@dgourillon1.joonix.net"
-  password      = var.default_password
-
-  name {
-    family_name = "team-a-admin-user"
-    given_name  = "team-a-admin-user"
+    family_name = "${each.key}"
+    given_name  = "${each.key}"
   }
 
   recovery_email = "dgourillon@google.com"
